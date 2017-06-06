2da9acc304685c06e162223fe13eace37da70b1b
jfcumsille <jfcumsille@uc.cl>
2017-06-06 18:12:38 -0400

    trying to fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,60 +7,28 @@ Rails.application.routes.draw do
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
   mount Spree::Core::Engine, at: '/'
 
-  #post 'pagoWeb' => 'orders#pagoWeb', as: :pagoWeb
-  
-          # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get '/locale/set', to: 'locale#set'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # non-restful checkout stuff
+  patch '/checkout/update/:state', to: 'checkout#update', as: :update_checkout
+  get '/checkout/:state', to: 'checkout#edit', as: :checkout_state
+  get '/checkout', to: 'checkout#edit', as: :checkout
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get '/orders/populate', to: 'orders#populate_redirect'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  resources :orders, except: [:index, :new, :create, :destroy] do
+    post :populate, on: :collection
+  end
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  get '/cart', to: 'orders#edit', as: :cart
+  patch '/cart', to: 'orders#update', as: :update_cart
+  put '/cart/empty', to: 'orders#empty', as: :empty_cart
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  # route globbing for pretty nested taxon and product paths
+  get '/t/*id', to: 'taxons#show', as: :nested_taxons
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get '/unauthorized', to: 'home#unauthorized', as: :unauthorized
+  get '/content/cvv', to: 'content#cvv', as: :cvv
+  get '/content/*path', to: 'content#show', as: :content
+  get '/cart_link', to: 'store#cart_link', as: :cart_link
 end
